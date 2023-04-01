5121946b9969853b65bc500753d51eeaa4b4f9bb
Carlos Miguel Soto <miguelsotocarlos@gmail.com>
2023-04-01 10:58:59 -0300

    add test for precompiled-checker task

--- scripts/src/oia/tests.py
+++ scripts/src/oia/tests.py
@@ -100,6 +100,47 @@ class OiaTests(unittest.TestCase):
         # max_score * score_multiplier
         self.assertEqual(resp["score"], 8)
 
+    def test_submission_frutales(self):
+        Database.populate_with_contests(["frutales"])
+        Oia.start()
+        Cms.start()
+
+        with open(Config.TASK_PATH / 'frutales.cpp', "rb") as f:
+            source = f.read()
+
+        resp = Oia.post(f'/user/create', json={
+            "username": "test_user",
+            "password": "test_pass",
+            "school": "escuela",
+            "email": "lala@lala.com",
+            "name": "Carlos",
+        }).json()
+        uid = resp["user_id"]
+        Oia.set_access_token(resp["token"])
+        resp = Oia.post(f'/submission/create', json={
+            "task_id": 1,
+            "user_id": resp["user_id"],
+            "sources": {
+                "frutales.%l": base64.b64encode(source).decode('utf-8')
+            }
+        })
+        self.assertEqual(resp.status_code, 200)
+
+        def submission_ready():
+            submissions = Oia.post('/submissions/get', json={"user_id": uid, "task_id": 1}).json()["submissions"]
+            return len(submissions) > 0 and submissions[0]["submission_status"] == "scored"
+
+        wait_for(submission_ready)
+
+        submission = Oia.post('/submissions/get', json={"user_id": uid, "task_id": 1}).json()["submissions"][0]
+        print(submission)
+
+        self.assertEqual(submission["result"]["score"], {"score": 95, "max_score": 100})
+
+        resp = Oia.post(f'/user/get', json={"user_id": uid}).json()
+        # max_score * score_multiplier
+        self.assertEqual(resp["score"], 95*4)
+
     def test_get_task(self):
         Database.populate_with_contests(["envido"])
         Oia.start()
