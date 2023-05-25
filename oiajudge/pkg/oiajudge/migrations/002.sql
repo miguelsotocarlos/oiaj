b741a6d0dcbce01e561bf85ea1b0836ce9a9fe24
Carlos Miguel Soto <miguelsotocarlos@gmail.com>
2023-05-25 12:21:13 -0300

    drop triggers before updates

--- oiajudge/pkg/oiajudge/migrations/002.sql
+++ oiajudge/pkg/oiajudge/migrations/002.sql
@@ -1,22 +1,32 @@
-TRUNCATE TABLE oia_tokens
-
-;;
+ALTER TABLE oia_task_score
+DISABLE TRIGGER ALL;;
 
 UPDATE oia_task_score
-SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_task_score.user_id)
+SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_task_score.user_id);;
 
-;;
+ALTER TABLE oia_task_score
+ENABLE TRIGGER ALL;;
+
+
+
+ALTER TABLE oia_tokens
+DISABLE TRIGGER ALL;;
 
 UPDATE oia_tokens oia_tokens
-SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_tokens.user_id)
+SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_tokens.user_id);;
 
-;;
+ALTER TABLE oia_tokens
+ENABLE TRIGGER ALL;;
 
+
+
+ALTER TABLE oia_user
+DISABLE TRIGGER ALL;;
 
 UPDATE oia_user
-SET id = oia_user.cms_user_id
+SET id = oia_user.cms_user_id;;
 
-;;
+ALTER TABLE oia_user DROP COLUMN cms_user_id;;
 
-ALTER TABLE oia_user DROP COLUMN cms_user_id;
-
+ALTER TABLE oia_user
+ENABLE TRIGGER ALL
