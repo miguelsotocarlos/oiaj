0c1937010d0e0ddc616efdb6fcf046b0c8d9a4ca
Carlos Miguel Soto <miguelsotocarlos@gmail.com>
2023-05-25 12:48:05 -0300

    delete and recreate primary and foreign key cosntraints

--- oiajudge/pkg/oiajudge/migrations/002.sql
+++ oiajudge/pkg/oiajudge/migrations/002.sql
@@ -1,32 +1,32 @@
-ALTER TABLE oia_task_score
-DISABLE TRIGGER ALL;;
+ALTER TABLE oia_task_score DROP CONSTRAINT oia_task_score_pkey;;
 
 UPDATE oia_task_score
 SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_task_score.user_id);;
 
-ALTER TABLE oia_task_score
-ENABLE TRIGGER ALL;;
+ALTER TABLE oia_task_score ADD PRIMARY KEY (user_id, task_id);;
 
 
-
-ALTER TABLE oia_tokens
-DISABLE TRIGGER ALL;;
-
-UPDATE oia_tokens oia_tokens
-SET user_id = (SELECT cms_user_id FROM oia_user WHERE oia_user.id = oia_tokens.user_id);;
-
-ALTER TABLE oia_tokens
-ENABLE TRIGGER ALL;;
+TRUNCATE TABLE oia_tokens;;
 
 
-
-ALTER TABLE oia_user
-DISABLE TRIGGER ALL;;
+ALTER TABLE oia_user DROP CONSTRAINT oia_user_pkey CASCADE;
 
 UPDATE oia_user
 SET id = oia_user.cms_user_id;;
 
 ALTER TABLE oia_user DROP COLUMN cms_user_id;;
 
-ALTER TABLE oia_user
-ENABLE TRIGGER ALL
+ALTER TABLE oia_user ADD PRIMARY KEY (id);;
+
+ALTER TABLE oia_submissions ADD CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id)
+            REFERENCES oia_user(id);;
+
+
+ALTER TABLE oia_tokens ADD CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id)
+            REFERENCES oia_user(id);;
+
+ALTER TABLE oia_task_score ADD CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id)
+            REFERENCES oia_user(id)
